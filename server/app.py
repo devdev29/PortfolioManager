6fc0b8d43ffe2df11ae689a7fe4995abbadeb55c
Devansh <devanshjoshi39@gmail.com>
2024-08-08 04:04:37 +0000

    added value controller

--- server/app.py
+++ server/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from load_dotenv import load_dotenv
 
 from controller.stock_controller import stocks
+from controller.value_controller import value
 
 load_dotenv('.env')
 
 app = Flask(__name__)
 
 app.register_blueprint(stocks, url_prefix='/stocks')
+app.register_blueprint(value, url_prefix='/value')
 
 if __name__ == '__main__':
     app.run(debug=True)
