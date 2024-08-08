14c50a6fbcb1cb8decc7bb17f49ce996f205645e
Devansh <devanshjoshi39@gmail.com>
2024-08-08 02:14:29 +0000

    added returns endpoints

--- server/controller/stock_controller.py
+++ server/controller/stock_controller.py
@@ -6,14 +6,14 @@ from datetime import date
 from flask import Blueprint, jsonify, request
 
 from model.stock_model import Stock
-from exceptions import InsufficientFundsError
+from exceptions import InsufficientFundsError, StockDoesNotExistError
 from repository.stock_repo import StockRepo
 from repository.account_repo import AccountRepo
 from repository.value_repo import ValueRepo
 
 stocks = Blueprint('stocks', __name__)
 
-STOCK_EXCHANGE = 'BSE'
+STOCK_EXCHANGE = 'nasdaq'
 
 #Helpers
 def update_flows(amount: float, account_no: str):
@@ -31,7 +31,7 @@ def update_flows(amount: float, account_no: str):
 def get_valid_stocks(ticker_search: str):
     ticker_search = ticker_search.upper()
     res = requests.get(
-        f'https://financialmodelingprep.com/api/v3/search?query={ticker_search}&exchange={STOCK_EXCHANGE}&apikey={os.environ["FMP_API_KEY"]}'
+        f'https://financialmodelingprep.com/api/v3/search?query={ticker_search}&apikey={os.environ["FMP_API_KEY"]}'
         ).json()
     return jsonify(res)
 
@@ -53,9 +53,9 @@ def add_stock():
         data = request.json
         ticker = data['ticker']
         price_res = requests.get(
-            f'https://api.twelvedata.com/price?symbol={ticker}&exchange={STOCK_EXCHANGE}&apikey={os.environ["TWELVE_API_KEY"]}'
+            f'https://api.twelvedata.com/price?symbol={ticker}&apikey={os.environ["TWELVE_API_KEY"]}'
         ).json()
-        amount = float(price_res['price'])*float(data['quantity'])
+        amount = float(price_res['price'])*int(data['quantity'])
         #Update account state and flows due to stock addition
         update_flows(amount=amount, account_no=data['account_no'])
         data['amount_invested'] = amount
@@ -75,7 +75,7 @@ def delete_stock(ticker: str):
     try:
         stock = StockRepo.get_stock_by_ticker(ticker)[0]
         price_res = requests.get(
-            f'https://api.twelvedata.com/price?symbol={ticker}&exchange={STOCK_EXCHANGE}&apikey={os.environ["TWELVE_API_KEY"]}'
+            f'https://api.twelvedata.com/price?symbol={ticker}&apikey={os.environ["TWELVE_API_KEY"]}'
         ).json()
         amount = price_res['price']*stock['quantity']
         update_flows(amount, stock.account_no)
@@ -90,9 +90,9 @@ def update_stock():
     try:
         data = request.json
         price_res = requests.get(
-            f'https://api.twelvedata.com/price?symbol={data['ticker']}&exchange={STOCK_EXCHANGE}&apikey={os.environ["TWELVE_API_KEY"]}'
+            f'https://api.twelvedata.com/price?symbol={data["ticker"]}&apikey={os.environ["TWELVE_API_KEY"]}'
         ).json()
-        amount = price_res['price']*data['quantity']
+        amount = float(price_res['price'])*int(data['quantity'])
         data['amount_invested'] = amount
         update_flows(amount, data['account_no'])
         stock = Stock(**data)
@@ -102,7 +102,38 @@ def update_stock():
         logging.exception(e)
         return {'message': 'could not update stock'}, 409
 
-@stocks.route('/performance', methods=['GET'])
+@stocks.route('/portfolio/returns', methods=['GET'])
+def get_total_returns():
+    total_returns = StockRepo.get_total_returns()
+    return {'returns': total_returns}, 200
+
+@stocks.route('/portfolio/returns/<ticker>', methods=['GET'])
+def get_returns(ticker: str):
+    try:
+        stock_returns = StockRepo.get_stock_returns(ticker)
+        return jsonify({'returns':stock_returns}), 200
+    except StockDoesNotExistError as e:
+        return jsonify({'message':str(e)}), 400
+    except Exception as e:
+        logging.exception(e)
+        return jsonify({'message':'Could not get returns'}), 409
+
+@stocks.route('/portfolio/performance', methods=['GET'])
 def get_stock_performance():
     #TODO: find api that gives stock performance
-    ...
+    performance={'gainers':[], 'losers':[]}
+    all_stocks = StockRepo.get_all_stocks()
+    for stock in all_stocks:
+        ticker = stock['ticker']
+        stock_quote = requests.get(
+            f'https://api.twelvedata.com/quote?symbol={ticker}&apikey={os.environ["TWELVE_API_KEY"]}'
+        ).json()
+        float_percent = float(stock_quote['percent_change'])
+        if float_percent > 0:
+            performance['gainers'].append({ticker: float_percent})
+        else:
+            performance['losers'].append({ticker: float_percent})
+        
+    performance['gainers'] = sorted(performance['gainers'], key = lambda gainer: float(list(gainer.values())[0]))
+    performance['losers'] = sorted(performance['losers'], key = lambda loser: float(list(loser.values())[0]), reverse=True)
+    return performance, 200
