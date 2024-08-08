c814b065f672450b7ab37800f22dc1784a251f5d
Devansh <devanshjoshi39@gmail.com>
2024-08-08 13:52:11 +0000

    added account blueprint

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from load_dotenv import load_dotenv
 
 from controller.stock_controller import stocks
 from controller.value_controller import value
+from controller.account_controller import account
 
 load_dotenv('.env')
 
@@ -10,6 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(stocks, url_prefix='/stocks')
 app.register_blueprint(value, url_prefix='/value')
+app.register_blueprint(account, url_prefix='/accounts')
 
 if __name__ == '__main__':
     app.run(debug=True)
