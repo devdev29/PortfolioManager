53ea6b2253cb3ae48731faab5fe959b4b401d068
Priyanjit <priyanjitpodder@gmail.com>
2024-08-09 07:32:38 +0000

    added mutual funds exceptions

--- server/exceptions.py
+++ server/exceptions.py
@@ -15,4 +15,16 @@ class StockDoesNotExistError(Exception):
         self.stock = stock
     def __str__(self) -> str:
         return f'Stock {self.stock} does not exist in the portfolio.'
+
+class MutualFundAlreadyExistsError(Exception):
+    def __init__(self, mutual_fund):
+        self.mutual_fund = mutual_fund
+    def __str__(self) -> str:
+        return f'Mutual Fund {self.mutual_fund} already exists in portfolio.'
+
+class MutualFundDoesNotExistError(Exception):
+    def __init__(self, mutual_fund):
+        self.mutual_fund = mutual_fund
+    def __str__(self) -> str:
+        return f'Mutual Fund {self.mutual_fund} does not exist in the portfolio.'
     
