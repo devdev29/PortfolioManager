87062eee0faf16fe81be86389fbf321fc55cd191
Priyanjit <priyanjitpodder@gmail.com>
2024-08-09 11:02:45 +0000

    fixed merge conflict

--- server/app.py
+++ server/app.py
@@ -3,24 +3,19 @@ from flask_cors import CORS
 from load_dotenv import load_dotenv
 
 from controller.stock_controller import stocks
-<<<<<<< Updated upstream
 from controller.value_controller import value
 from controller.account_controller import account
+from controller.mutual_funds_controller import mutual_funds
 
 load_dotenv('.env')
-=======
-from controller.mutual_funds_controller import mutual_funds
->>>>>>> Stashed changes
+
 
 app = Flask(__name__)
 CORS(app=app, origins=['*'])
 
 app.register_blueprint(stocks, url_prefix='/stocks')
-<<<<<<< Updated upstream
 app.register_blueprint(value, url_prefix='/value')
-=======
 app.register_blueprint(mutual_funds, url_prefix='/mutual_funds')
->>>>>>> Stashed changes
 app.register_blueprint(account, url_prefix='/accounts')
 
 if __name__ == '__main__':
