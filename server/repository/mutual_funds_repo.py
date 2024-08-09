fba3a708b054047f5f9e67794d4a93ee52f92c99
Priyanjit <priyanjitpodder@gmail.com>
2024-08-09 07:32:08 +0000

    repository merge fix

--- server/repository/mutual_funds_repo.py
+++ server/repository/mutual_funds_repo.py
@@ -1,4 +1,8 @@
+import requests
+import os
+
 from repository.database_access import get_db_connection
+from exceptions import MutualFundAlreadyExistsError, MutualFundDoesNotExistError
 from model.mutual_funds_model import MutualFunds
 
 class MutualFundsRepo:
@@ -14,24 +18,47 @@ class MutualFundsRepo:
     @staticmethod
     def search_mutual_funds_by_id(mf_id: int):
         with get_db_connection() as (_, cursor):
-            stmt = 'select * from mutual_funds where mf_id like %d%'
-            cursor.execute(stmt, params=(mf_id))
+            stmt = 'select * from stocks where mf_id like concat(%d, "%")'
+            cursor.execute(stmt, params=(mf_id,))
             mutual_funds = cursor.fetchall()
             return mutual_funds
     
     @staticmethod
     def get_mutual_funds_by_id(mf_id: int):
         with get_db_connection() as (_, cursor):
-            stmt = 'select * from mutual_funds where mf_id=%d%'
-            params = (mf_id)
+            stmt = 'select * from mutual_funds where mf_id=%d'
+            params = (mf_id,)
             cursor.execute(stmt, params=params)
             mutual_funds = cursor.fetchone()
             return mutual_funds
     
     @staticmethod
+    def get_mutual_funds_returns(mf_id: str):
+        mutual_funds = MutualFundsRepo.get_mutual_funds_by_id(mf_id)
+        if not mutual_funds:
+            raise MutualFundDoesNotExistError(mf_id)
+        price_res = requests.get(
+        f'https://api.mfapi.in/mf/{mf_id}/latest'
+        ).json()
+        curr_price = float(price_res['data'][0]['nav'])*int(mutual_funds['quantity'])
+        mutual_funds_return = float(mutual_funds['amount_invested'])-curr_price
+        return mutual_funds_return
+    
+    @staticmethod
+    def get_total_returns():
+        total_returns = 0
+        mutual_funds = MutualFundsRepo.get_all_mutual_funds()
+        for mf in mutual_funds:
+            total_returns += MutualFundsRepo.get_mutual_funds_returns(mf['ticker'])
+        return total_returns
+    
+    @staticmethod
     def add_new_mutual_funds(mutual_funds: MutualFunds):
         with get_db_connection() as (conn, cursor):
-            stmt = 'insert into mutual_funds values(%s, %s, %s, %s, %s)'
+            exists = MutualFundsRepo.get_mutual_funds_by_id(mutual_funds.mf_id)
+            if exists:
+                raise MutualFundAlreadyExistsError(mutual_funds.ticker)
+            stmt = 'insert into mutual_funds values(%s, %s, %s, %s, %s, %s)'
             params = mutual_funds.to_list()
             cursor.execute(stmt, params=params)
             conn.commit()
@@ -41,13 +68,12 @@ class MutualFundsRepo:
     @staticmethod
     def update_mutual_funds(mutual_funds: MutualFunds):
         with get_db_connection() as (conn, cursor):
-            stmt = 'update mutual_funds set values(%s, %s, %s, %s, %s)'
+            stmt = 'update mutual_funds set values(%s, %s, %s, %s, %s, %s)'
             params = mutual_funds.to_list()
             cursor.execute(stmt, params=params)
             conn.commit()
             affected_rows = cursor.rowcount
-                
-
+            return affected_rows
     
     @staticmethod
     def remove_mutual_funds(mf_id: int):
