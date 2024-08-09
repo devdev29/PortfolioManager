0bebc1ec85f8eab0b2f0b68da59c74d63435a4ad
Priyanjit <priyanjitpodder@gmail.com>
2024-08-09 07:30:12 +0000

    controller merge fix

--- server/controller/mutual_funds_controller.py
+++ server/controller/mutual_funds_controller.py
@@ -1,73 +1,145 @@
 import os
 import requests
+import logging
+from datetime import date
 
 from flask import Blueprint, jsonify, request
 
 from model.mutual_funds_model import MutualFunds
 from repository.mutual_funds_repo import MutualFundsRepo
+from repository.account_repo import AccountRepo
+from repository.value_repo import ValueRepo
+from exceptions import InsufficientFundsError, MutualFundDoesNotExistError
 
 mutual_funds = Blueprint('mutual_funds', __name__)
 
+#Helpers
+def update_flows(amount: float, account_no: str):
+    AccountRepo.update_amount(account_no, amount)
+    #Update total cash flows
+    value_row = ValueRepo.get_value(date.today())[0]
+    inflow = value_row['inflow']
+    outflow = value_row['outflow']
+    inflow += amount
+    outflow += amount
+    ValueRepo.update_inflow(inflow) 
+    ValueRepo.update_outflow(outflow)
 
-@mutual_funds.route('/mutual_funds/search/', methods=['GET'])
+
+@mutual_funds.route('/search/', methods=['GET'])
 def get_valid_mutual_funds():
     res = requests.get(
         f'https://api.mfapi.in/mf'
         )
     #print(type(res.text))
-    return res.text
+    return jsonify(res)
 
-# @mutual_funds.route('/mutual_funds/search/<mutual_funds_id>', methods=['GET'])
-# def get_valid_mutual_funds(mf_id: int):
-#     mutual_funds = MutualFundsRepo.search_mutual_funds_by_id(mf_id)
-#     mf_id = 
-#     res = requests.get(
-#         f'https://api.mfapi.in/mf/{mf_id}/latest'
-#         )
-#     return jsonify(mutual_funds), 200
+@mutual_funds.route('/search/<mf_id>', methods=['GET'])
+def get_valid_mutual_funds(mf_id: int):
+    mutual_funds = MutualFundsRepo.search_mutual_funds_by_id(mf_id)
+    res = requests.get(
+        f'https://api.mfapi.in/mf/{mf_id}/latest'
+        )
+    return jsonify(res), 200
 
-@mutual_funds.route('/mutual_funds/portfolio/<mutual_funds_id>', methods=['GET'])
-def get_portfolio_mutual_funds(mutual_funds_id: int):
-    mutual_funds = MutualFundsRepo.search_mutual_funds_by_id(mutual_funds_id)
+@mutual_funds.route('/portfolio/<mf_id>', methods=['GET'])
+def get_portfolio_mutual_funds(mf_id: int):
+    mutual_funds = MutualFundsRepo.search_mutual_funds_by_id(mf_id)
     return jsonify(mutual_funds), 200
 
-@mutual_funds.route('/mutual_funds/portfolio', methods=['POST'])
+@mutual_funds.route('/portfolio/all', methods=['GET'])
+def get_all_mutual_funds():
+    mutual_funds = MutualFundsRepo.get_all_mutual_funds()
+    return jsonify(mutual_funds), 200
+
+@mutual_funds.route('/portfolio', methods=['POST'])
 def add_mutual_funds():
-    # TODO: add logic to calculate and adjust cash flow from given account
-    data = request.json
-    mf_id = data['mf_id']
-    price_res = requests.get(
+    try:
+        data = request.json
+        mf_id = data['mf_id']
+        price_res = requests.get(
         f'https://api.mfapi.in/mf/{mf_id}/latest'
-    ).json()
-    print(price_res['data'][0]['nav'])
-    amount = price_res['data'][0]['nav']*data['quantity']
-    data['amount_invested'] = amount
-    new_mf = MutualFunds(**data)       
-    added = MutualFundsRepo.add_new_mutual_fund(new_mf)
-    if added == 1:
-        return jsonify(new_mf), 200
-    return {'message': 'could not insert stock'}, 400
+        ).json()
+        amount = float(price_res['data'][0]['nav'])*int(data['quantity'])
+        #Update account state and flows due to stock addition
+        update_flows(amount=amount, account_no=data['account_no'])
+        data['amount_invested'] = amount
+        new_mf = MutualFunds(**data)
+        MutualFundsRepo.add_new_stock(new_mf)
+        return jsonify(new_mf), 201
+    except InsufficientFundsError as e:
+        return jsonify({'message': str(e)}), 400
+    except Exception as e:
+        logging.exception(e)
+        return {'message': 'could not insert mutual fund'}, 409
 
 
 @mutual_funds.route('/mutual_funds/portfolio/<mf_id>', methods=['DELETE'])
 def delete_mutual_funds(mf_id: int):
     # TODO: add logic to update liquid balance in account
-    res = MutualFundsRepo.remove_stock(mf_id)
-    if res == 1:
-        return jsonify(mf_id), 200
-    return {'message': 'could not remove stock'}, 500
+    try:
+        mutual_funds = MutualFundsRepo.get_mutual_funds_by_id(mf_id)[0]
+        price_res = requests.get(
+            f'https://api.mfapi.in/mf/{mf_id}/latest'
+        ).json()
+        amount = float(price_res['data'][0]['nav'])*int(mutual_funds['quantity'])
+        update_flows(amount, mutual_funds.account_no)
+        MutualFundsRepo.remove_stock(mf_id)
+        return jsonify(mf_id), 202
+    except Exception as e:
+        logging.exception(e)
+        return jsonify({'message': 'could not remove mutual fund'}), 409
 
-@mutual_funds.route('/mutual_funds/portfolio', methods=['PUT'])
+@mutual_funds.route('/portfolio', methods=['PUT'])
 def update_mutual_funds():
-    data = request.json
-    mf_id = data['mf_id']
-    price_res = requests.get(
-        f'https://api.mfapi.in/mf/{mf_id}/latest'
-    ).json()
-    amount = price_res['data'][0]['nav']*data['quantity']
-    data['amount_invested'] = amount
-    mutual_funds = MutualFunds(**data)
-    res = MutualFundsRepo.update_mutual_funds(mutual_funds=mutual_funds)
-    if res == 1:
-        return jsonify(res), 200
-    return {'message': 'could not update stock'}, 500
+    try:
+        data = request.json
+        mf_id = data['mf_id']
+        price_res = requests.get(
+            f'https://api.mfapi.in/mf/{mf_id}/latest'
+        ).json()
+        amount = float(price_res['data'][0]['nav'])*int(data['quantity'])
+        data['amount_invested'] = amount
+        update_flows(amount, data['account_no'])
+        mutual_funds = MutualFunds(**data)
+        MutualFundsRepo.update_stock(mutual_funds=mutual_funds)
+        return jsonify(mutual_funds), 204
+    except Exception as e:
+        logging.exception(e)
+        return {'message': 'could not update stock'}, 409
+
+@mutual_funds.route('/portfolio/returns', methods=['GET'])
+def get_total_returns():
+    total_returns = MutualFundsRepo.get_total_returns()
+    return {'returns': total_returns}, 200
+
+@mutual_funds.route('/portfolio/returns/<mf_id>', methods=['GET'])
+def get_returns(mf_id: int):
+    try:
+        mutual_funds_returns = MutualFundsRepo.get_stock_returns(mf_id)
+        return jsonify({'returns':mutual_funds_returns}), 200
+    except MutualFundDoesNotExistError as e:
+        return jsonify({'message':str(e)}), 400
+    except Exception as e:
+        logging.exception(e)
+        return jsonify({'message':'Could not get returns'}), 409
+
+@mutual_funds.route('/portfolio/performance', methods=['GET'])
+def get_stock_performance():
+    #TODO: find api that gives stock performance
+    performance={'gainers':[], 'losers':[]}
+    all_mutual_funds = MutualFundsRepo.get_all_stocks()
+    for mutual_funds in all_mutual_funds:
+        mf_id = mutual_funds['mf_id']
+        mutual_funds_quote = requests.get(
+            f'https://api.mfapi.in/mf/{mf_id}/latest'
+        ).json()
+        float_percent = float(mutual_funds_quote['percent_change'])
+        if float_percent > 0:
+            performance['gainers'].append({mf_id: float_percent})
+        else:
+            performance['losers'].append({mf_id: float_percent})
+        
+    performance['gainers'] = sorted(performance['gainers'], key = lambda gainer: float(list(gainer.values())[0]))
+    performance['losers'] = sorted(performance['losers'], key = lambda loser: float(list(loser.values())[0]), reverse=True)
+    return performance, 200
