bb7029d04a6399afe19750a4c13a3b1be14809dc
Devansh <devanshjoshi39@gmail.com>
2024-08-10 20:09:09 +0000

    added transactions repository

--- server/repository/database_access.py
+++ server/repository/database_access.py
@@ -2,10 +2,11 @@ import os
 
 from contextlib import contextmanager
 from psycopg2 import connect
-from psycopg2.extras import RealDictCursor
+from psycopg2.extras import RealDictCursor, register_uuid
 from load_dotenv import load_dotenv
 
 load_dotenv('.env')
+register_uuid()
 
 @contextmanager
 def get_db_connection():
